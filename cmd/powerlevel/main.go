c483e4569e356571bbdec63e298adfa8c9854286
Benjamin Glusa <benjamin.glusa@volkswagen.de>
2023-05-09 20:45:14 +0200

    Add queries for kwh today/month/year and total

--- cmd/powerlevel/main.go
+++ cmd/powerlevel/main.go
@@ -12,6 +12,15 @@ func main() {
 	defer db.Close()
 	db.CreateTableIfNotExits()
 
+	kwhToday := db.KwhToday()
+	kwhThisMonth := db.KwhThisMonth()
+	KwhThisYear := db.KwhThisYear()
+	KwhTotal := db.KwhTotal()
+	fmt.Printf("kwh today: %.3f \n", kwhToday)
+	fmt.Printf("kwh this month: %.3f \n", kwhThisMonth)
+	fmt.Printf("kwh this year: %.3f \n", KwhThisYear)
+	fmt.Printf("kwh total: %.3f \n", KwhTotal)
+
 	fmt.Println("all done")
 
 }
--- pkg/adapter/database.go
+++ pkg/adapter/database.go
@@ -8,6 +8,12 @@ import (
 )
 
 type DatabaseAdapter interface {
+	AddMeasurement(watt uint16)
+	KwhToday() float32
+	KwhThisMonth() float32
+	KwhThisYear() float32
+	KwhTotal() float32
+
 	CreateTableIfNotExits()
 	Close()
 }
@@ -17,13 +23,65 @@ type MySqlAdapter struct {
 }
 
 func (m *MySqlAdapter) CreateTableIfNotExits() {
-	result, err := m.db.Query("CREATE TABLE if not exists foo (time VARCHAR(5), power INT);")
+	result, err := m.db.Query("CREATE TABLE if not exists `power` (`time` timestamp NOT NULL UNIQUE DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, `watt` int NOT NULL);")
 	if err != nil {
 		panic(err)
 	}
 
 	defer result.Close()
 }
+
+func (m *MySqlAdapter) AddMeasurement(watt uint16) {
+	query := fmt.Sprintf("INSERT INTO `power` (`time`, `watt`) VALUES (now(), %d);", watt)
+	result, err := m.db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+
+	defer result.Close()
+}
+
+func (m *MySqlAdapter) KwhToday() float32 {
+	var kWh float32
+	err := m.db.QueryRow("SELECT ROUND(AVG(watt) / 1000,3) as kWh FROM power WHERE DATE(time) = CURDATE();").Scan(&kWh)
+	if err != nil {
+		panic(err)
+	}
+
+	return kWh
+}
+
+
+func (m *MySqlAdapter) KwhThisMonth() float32 {
+	var kWh float32
+	err := m.db.QueryRow("SELECT ROUND(AVG(watt) / 1000,3) as kWh FROM power WHERE UNIX_TIMESTAMP(time) BETWEEN UNIX_TIMESTAMP(LAST_DAY(CURDATE()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND UNIX_TIMESTAMP(LAST_DAY(CURDATE()) + INTERVAL 1 DAY);").Scan(&kWh)
+	if err != nil {
+		panic(err)
+	}
+
+	return kWh
+}
+
+func (m *MySqlAdapter) KwhThisYear() float32 {
+	var kWh float32
+	err := m.db.QueryRow("SELECT ROUND(AVG(watt) / 1000,3) as kWh FROM power WHERE DATE(time) BETWEEN MAKEDATE(year(now()),1) AND MAKEDATE(year(now()),1) + interval 1 year - interval 1 day;").Scan(&kWh)
+	if err != nil {
+		panic(err)
+	}
+
+	return kWh
+}
+
+func (m *MySqlAdapter) KwhTotal() float32 {
+	var kWh float32
+	err := m.db.QueryRow("SELECT ROUND(AVG(watt) / 1000,3) as kWh FROM power;").Scan(&kWh)
+	if err != nil {
+		panic(err)
+	}
+
+	return kWh
+}
+
 
 func (m *MySqlAdapter) Close() {
 	defer m.db.Close()
